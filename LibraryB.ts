bde929b87df2470b576a1b7802be10ad1d328db7
Chris Eadie <caeadie@hotmail.com>
2023-02-11 16:13:37 +0000

    add logger to Lib B

--- LibraryB.ts
+++ LibraryB.ts
@@ -1,14 +1,16 @@
 import { Injectable } from "./decorators"
 import { LibraryA } from "./LibraryA"
+import { Logger } from "./Logger"
 
 @Injectable()
 export class LibraryB {
-    constructor(private libA?:LibraryA){
+    constructor(private libA?:LibraryA,private logger?:Logger){
         
     }
 
     public getMoreData(){
         var b = this.libA.getData()
+        this.logger.execute<string[]>(b);
         return {
             dataA:[1,2,3],
             context:b
